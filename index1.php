fd37533c2d84771166bbaab5eb96bd77bf9c8bea
4N1M4C1T0 <richardmeneses2110@gmail.com>
2020-07-02 11:42:45 -0500

    Tarea 2

--- index1.php
+++ index1.php
@@ -1,22 +1,8 @@
 <?php
+$color = array(4 => 'Blanco', 6 => 'Verde', 11=> 'roja');
 
-$color = array(4, 6, 11);
-
-$longitud=count($color);
-
-for($i=0;$i<$longitud;$i++){
-    if($color[$i] == 4){
-        echo "Blanco";
-    }
-}
+foreach($color as $colors){
+    echo $colors;
+break;
+} 
 ?>
-
-<!--comment
-<?php
-$color = array(4 => 'Blanco', 6 => 'Verde', 11=> 'Roja');
-for ($i=0;$i<1;$i++)
-{
-    echo $color[4];
-}
-?>
--->
--- index5.php
+++ index5.php
@@ -1,20 +1,8 @@
 <?php
-$color = array(4 => 'Blanco', 6 => 'Verde', 11=> 'Roja');
-for ($i=0;$i<1;$i++)
-{
-    echo $color[4];
-}
-?>
+$color = array(4 => 'blanco', 6 => 'verde', 11=> 'roja');
+foreach($color as $colores){
+    echo $colores;
+break;
+} 
 
-<?php
-
-$color = array(4 => 'Blanco', 6 => 'Verde', 11=> 'Roja');
-
-$longitud=count($color);
-
-for($i=0;$i<$longitud;$i++){
-    if($color[$i] == $color[4]){
-        echo $color[4];
-    }
-}
 ?>
